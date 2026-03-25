8889e5e9fa9d6f4ec59e1baefdf6c6436a27aa2e
Lam Nguyen <lamtnguyen989@gmail.com>
2026-03-25 17:18:25 -0500

    pushing generated config

--- enable-network.sh
+++ enable-network.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-# Shell script for documenting how to get network working (still have no idea atp :3)
+###
+# Shell script for documenting how to get network working a.k.a getting an IP
+###
+
+# Environment setup
+set -e
+source network.env
+
+# Getting meshtastic CLI
+pip install -r requirements.txt
